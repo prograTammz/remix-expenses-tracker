ccda6bc9a567ab10b064b8324b382b93e672c6c0
progratammz <ahmedtamer1998@gmail.com>
2024-04-13 21:16:27 +0200

    Added MainHeader Navigation Component

---
+++ app/components/navigation/MainHeader.tsx
@@ -0,0 +1,34 @@
+import { FunctionComponent } from "react";
+import Logo from "../util/Logo";
+
+const MainHeader: FunctionComponent = () => {
+  return (
+    <header id="main-header">
+      <Logo />
+      <nav id="main-nav">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/pricing">Pricing</a>
+          </li>
+          <li>
+            <a href="/expenses">Expenses</a>
+          </li>
+        </ul>
+      </nav>
+      <nav id="cta-nav">
+        <ul>
+          <li>
+            <a href="/auth" className="cta">
+              Login
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
